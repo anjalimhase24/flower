e70ca31b0862f3bd56714b9cd3d9c8c41f511d93
anjalimhase24 <anjalimhase24@gmail.com>
2026-04-04 21:02:22 +0530

    add navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,22 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter , Routes , Router } from 'react-router'; 
-import Home from './views/Home/Home';
-import About from './views/About/About';
-import Contact from './views/Contact/Contact';  
-import Services from './views/Services/Services';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './views/Home/Home'
+import About from './views/About/About'
+import Contact from './views/Contact/Contact'
+import Services from './views/Services/Services'
+import Navbar from './Components/Navbar/Navbar'
 
-const root = createRoot(document.getElementById('root')); 
+const root = createRoot(document.getElementById('root'))
 
 root.render(
-<BrowserRouter>
+  <BrowserRouter>
+    <Navbar />
     <Routes>
-        <Route path="/" element={<h1>Home</h1>} />  
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/services " element={<Services/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
-</BrowserRouter>,
-);
+  </BrowserRouter>,
+)
 
